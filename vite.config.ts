f1115ea06eb9b9200512ac58ce9e6a742c3a61a3
tibosi141 <2298203804@qq.com>
2023-04-23 17:51:44 +0800

    chore: add auto import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +11,17 @@ export default defineConfig({
       '~': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      // 配置需要自动导入的库
+      imports: ['vue'],
+      // 生成类型文件的地址
+      dts: 'types/auto-imports.d.ts',
+    }),
+    Components({
+      // 生成类型文件的地址
+      dts: 'types/components.d.ts',
+    }),
+  ],
 })
